05b2509d6c9fe093dac8d844b31fa3f9682ad640
Shane Scott <shaneradio@gmail.com>
2018-04-11 16:29:19 -0400

    added, populated array of OurClickers.js

---
+++ click-click-game-game/src/ourClickers.js
@@ -0,0 +1,28 @@
+const OurClickers = [
+    {
+        id: 1,
+        name: "Bill"
+    },
+    {
+        id: 2,
+        name: "Bill"
+    },
+    {
+        id: 3,
+        name: "Bill"
+    },
+    {
+        id: 4,
+        name: "Bill"
+    },
+    {
+        id: 5,
+        name: "Bill"
+    },
+    {
+        id: 6,
+        name: "Bill"
+    }
+];
+
+export default OurClickers;
